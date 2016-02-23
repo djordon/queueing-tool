17696ca5532c6b6d911520f6e366a609597c5764
djordon <dan.jordon@gmail.com>
2016-02-22 21:11:20 -0500

    Changed travis.yml

---
+++ queueing_tool/graph/graph_wrappers.py
@@ -0,0 +1,59 @@
+
+
+
+class GraphWrapper(object):
+
+    def __init__(self, g):
+        pass
+
+    def vertices(self):
+        pass
+
+    def vertice(self, v):
+        pass
+
+    def edges(self):
+        pass
+
+    def edge(self, e0, e1):
+        pass
+
+    def out_edges(self):
+        pass
+
+    def out_degree(self):
+        pass
+
+    def is_directed(self):
+        pass
+
+    def in_edges(self):
+        pass
+
+    def ep(self):
+        pass
+
+    def vp(self):
+        pass
+
+    @property
+    def edge_index(self):
+        pass
+
+    def copy(self):
+        pass
+
+    def new_edge_property(self):
+        pass
+
+    def new_edge_property(self):
+        pass
+
+    def num_edges(self):
+        pass
+
+    def num_vertices(self):
+        pass
+
+    def add_edge(self, source, target):
+        pass
