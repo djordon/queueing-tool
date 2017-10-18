40f2b1e3d979d801a69fb09253c7708fa61f78b2
djordon <daniel@seatgeek.com>
2017-10-18 15:35:16 -0400

    Bumped the version of the library

--- queueing_tool/__init__.py
+++ queueing_tool/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 from queueing_tool.queues import *
-import queueing_tool.queues
+import queueing_tool.queues as queues
 
 from queueing_tool.network import *
 import queueing_tool.network as network
@@ -10,11 +10,11 @@ from queueing_tool.graph import *
 import queueing_tool.graph as graph
 
 __all__ = []
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 
 __all__.extend(['__version__'])
 __all__.extend(queues.__all__)
 __all__.extend(network.__all__)
 __all__.extend(graph.__all__)
 
-#del queues, network, generation, graph
+# del queues, network, generation, graph
