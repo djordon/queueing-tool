be0d3aa97d5d8dcbd59ae95fe5911be0a70a0973
djordon <daniel@seatgeek.com>
2017-12-17 11:39:54 -0500

    Use pytest for testing

--- tests/test_graph_generation.py
+++ tests/test_graph_generation.py
@@ -1,42 +1,25 @@
-import unittest
-
-from numpy.random import randint
 import networkx as nx
 import numpy as np
+import pytest
 
 import queueing_tool as qt
 
 
 def generate_adjacency(a=3, b=25, c=6, n=12):
     ans = {}
-    for k in np.unique(randint(a, b, n)):
-        ans[k] = {j: {} for j in randint(a, b, randint(1, c))}
+    for k in np.unique(np.random.randint(a, b, n)):
+        ans[k] = {j: {} for j in np.random.randint(a, b, np.random.randint(1, c))}
     return ans
 
 
-class TestGraphFunctions(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        cls.expected_response0 = {
-            0: {1: {'edge_type': 5}},
-            1: {2: {'edge_type': 9}, 3: {'edge_type': 14}},
-            2: {0: {'edge_type': 1}},
-            3: {3: {'edge_type': 0}}
-        }
-        cls.expected_response1 = {
-            0: {1: {'edge_type': 5}},
-            1: {2: {'edge_type': 9}, 3: {'edge_type': 0}},
-            2: {0: {'edge_type': 1}},
-            3: {}
-        }
+class TestGraphFunctions(object):
 
     def test_graph2dict(self):
         adj = generate_adjacency()
         g1 = qt.adjacency2graph(adj, adjust=2)
         aj1 = qt.graph2dict(g1)
         g2 = qt.adjacency2graph(aj1, adjust=2)
-        self.assertTrue(nx.is_isomorphic(g1, g2))
+        assert nx.is_isomorphic(g1, g2)
 
     def test_add_edge_lengths(self):
         g1 = qt.generate_pagerank_graph(10)
@@ -46,18 +29,18 @@ class TestGraphFunctions(unittest.TestCase):
         for key in g2.edge_properties():
             edge_props.add(key)
 
-        self.assertTrue('edge_length' in edge_props)
+        assert 'edge_length' in edge_props
 
     def test_generate_transition(self):
         g = qt.generate_random_graph(20)
         mat = qt.generate_transition_matrix(g)
 
         ans = np.sum(mat, axis=1)
-        self.assertTrue(np.allclose(ans, 1))
+        np.testing.assert_allclose(ans, 1)
 
         mat = qt.generate_transition_matrix(g, seed=10)
         ans = np.sum(mat, axis=1)
-        self.assertTrue(np.allclose(ans, 1))
+        np.testing.assert_allclose(ans, 1)
 
     def test_adjacency2graph_matrix_adjacency(self):
 
@@ -69,9 +52,14 @@ class TestGraphFunctions(unittest.TestCase):
         ety = {0: {1: 5}, 1: {2: 9, 3: 14}}
 
         g = qt.adjacency2graph(adj, edge_type=ety, adjust=2)
-        ans = qt.graph2dict(g)
-
-        self.assertTrue(ans == self.expected_response1)
+        actual = qt.graph2dict(g)
+        expected = {
+            0: {1: {'edge_type': 5}},
+            1: {2: {'edge_type': 9}, 3: {'edge_type': 0}},
+            2: {0: {'edge_type': 1}},
+            3: {}
+        }
+        assert actual == expected
 
     def test_adjacency2graph_matrix_etype(self):
         # Test adjacency argument using ndarrays work
@@ -82,11 +70,17 @@ class TestGraphFunctions(unittest.TestCase):
                         [0, 0, 0, 0]])
 
         g = qt.adjacency2graph(adj, edge_type=ety, adjust=1)
-        ans = qt.graph2dict(g)
-        self.assertEqual(ans, self.expected_response0)
+        actual = qt.graph2dict(g)
+        expected = {
+            0: {1: {'edge_type': 5}},
+            1: {2: {'edge_type': 9}, 3: {'edge_type': 14}},
+            2: {0: {'edge_type': 1}},
+            3: {3: {'edge_type': 0}}
+        }
+        assert expected == actual
 
     def test_adjacency2graph_errors(self):
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             qt.adjacency2graph([])
 
     def test_set_types_random(self):
@@ -107,16 +101,16 @@ class TestGraphFunctions(unittest.TestCase):
         props = (np.array(mat).sum(1) + 0.0) / len(non_loops)
         ps = np.array([pType[k] for k in eType])
 
-        self.assertTrue(np.allclose(props, ps, atol=0.01))
+        np.testing.assert_allclose(props, ps, atol=0.01)
 
         prob[-1] = 2
         pType = {eType[k]: prob[k] for k in range(nT)}
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             g = qt.set_types_random(g, proportions=pType, seed=10)
 
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             g = qt.set_types_random(g, loop_proportions=pType, seed=10)
 
     def test_test_graph_importerror(self):
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             qt.generate_transition_matrix(1)
--- tests/test_statistical_properties.py
+++ tests/test_statistical_properties.py
@@ -1,18 +1,15 @@
 import functools
 import math
-import os
-import unittest
 
 import numpy as np
+import pytest
 
 import queueing_tool as qt
 
 
-TRAVIS_TEST = os.environ.get('TRAVIS_TEST', False)
-
-
 def empirical_cdf0(x, z, n):
     return np.sum(z <= x) / n
+
 
 empirical_cdf = np.vectorize(empirical_cdf0, excluded={1, 2})
 
@@ -20,23 +17,27 @@ empirical_cdf = np.vectorize(empirical_cdf0, excluded={1, 2})
 def chi2_cdf(q, k, n=1000000, ns=1):
     return np.mean([empirical_cdf(q, np.random.chisquare(k, n), n) for i in range(ns)])
 
-reason = "Test takes long."
+
+@pytest.fixture
+def lam():
+    return float(np.random.randint(1, 10))
 
 
-class TestQueueServers(unittest.TestCase):
+@pytest.fixture
+def rho():
+    return np.random.uniform(0.5, 1)
 
-    def setUp(self):
-        self.lam = np.random.randint(1, 10) + 0.0
-        self.rho = np.random.uniform(0.5, 1)
 
-    @unittest.skipIf(TRAVIS_TEST, reason)
-    def test_Markovian_QueueServer(self):
+@pytest.mark.slow
+class TestQueueServerStatistically(object):
+
+    def test_markovian_property(self, lam, rho):
 
         nSe = np.random.randint(1, 10)
-        mu = self.lam / (self.rho * nSe)
+        mu = lam / (rho * nSe)
 
         def arr(t):
-            return t + np.random.exponential(1 / self.lam)
+            return t + np.random.exponential(1 / lam)
 
         def ser(t):
             return t + np.random.exponential(1 / mu)
@@ -70,23 +71,22 @@ class TestQueueServers(unittest.TestCase):
 
         x, y = dep[1:], dep[:-1]
         cc = np.corrcoef(x, y)[0, 1]
-        self.assertAlmostEqual(cc, 0, 1)
-        self.assertGreater(p1, 0.05)
+        assert np.isclose(cc, 0, atol=1e-1)
+        assert p1 > 0.05
 
-    @unittest.skipIf(TRAVIS_TEST, reason)
-    def test_QueueServer_Littleslaw(self):
+    def test_littles_law(self, lam, rho):
 
         nSe = np.random.randint(1, 10)
-        mu = self.lam / (self.rho * nSe)
+        mu = lam / (rho * nSe)
 
         def arr(t):
-            return t + np.random.exponential(1 / self.lam)
+            return t + np.random.exponential(1 / lam)
 
         def ser(t):
             return t + np.random.exponential(1 / mu)
 
         q = qt.QueueServer(num_servers=nSe, arrival_f=arr, service_f=ser)
-        n = 500000
+        n = 250000
 
         q.set_active()
         q.simulate(n=n)    # Burn in period
@@ -97,20 +97,19 @@ class TestQueueServers(unittest.TestCase):
 
         ind = data[:, 2] > 0
         wait = data[ind, 1] - data[ind, 0]
-        ans = np.mean(wait) * self.lam - np.mean(data[:, 3]) * self.rho
+        ans = np.mean(wait) * lam - np.mean(data[:, 3]) * rho
 
-        self.assertAlmostEqual(ans, 0, 1)
+        assert np.isclose(ans, 0, atol=1e-1)
 
-    @unittest.skipIf(TRAVIS_TEST, reason)
-    def test_LossQueue_blocking(self):
+    def test_queue_blocking(self, lam, rho):
 
         nSe = np.random.randint(1, 10)
-        mu = self.lam / (self.rho * nSe)
+        mu = lam / (rho * nSe)
         k = np.random.randint(5, 15)
         scl = 1 / (mu * k)
 
         def arr(t):
-            return t + np.random.exponential(1 / self.lam)
+            return t + np.random.exponential(1 / lam)
 
         def ser(t):
             return t + np.random.gamma(k, scl)
@@ -127,62 +126,61 @@ class TestQueueServers(unittest.TestCase):
         nA1 = q2.num_arrivals[1]
         nB1 = q2.num_blocked
 
-        a = self.lam / mu
+        a = lam / mu
         f = np.array([math.factorial(j) for j in range(nSe + 1)])
 
         pois_pmf = np.exp(-a) * a**nSe / math.factorial(nSe)
         pois_cdf = np.sum(np.exp(-a) * a**np.arange(nSe + 1) / f)
         p_block = (nB1 - nB0 + 0.0) / (nA1 - nA0)
-        self.assertAlmostEqual(pois_pmf / pois_cdf, p_block, 2)
+
+        assert np.isclose(pois_pmf / pois_cdf, p_block, atol=1e-2)
 
 
-class TestRandomMeasure(unittest.TestCase):
+@pytest.mark.slow
+def test_poisson_random_measure():
+    # This function tests to make sure the poisson_random_measure function
+    # actually simulates a Poisson random measure. It does so looking for
+    # Poisson random variables using a chi-squared test (testing the
+    # composite null hypothesis). It does not look for independence of the
+    # random variables.
+    # This test should fail some percentage of the time
 
-    @unittest.skipIf(TRAVIS_TEST, reason)
-    def test_poisson_random_measure(self):
-        # This function tests to make sure the poisson_random_measure function
-        # actually simulates a Poisson random measure. It does so looking for
-        # Poisson random variables using a chi-squared test (testing the
-        # composite null hypothesis). It does not look for independence of the
-        # random variables.
-        # This test should fail some percentage of the time
+    def rate(t):
+        return 0.5 + 4 * np.sin(np.pi * t / 12)**2
 
-        def rate(t):
-            return 0.5 + 4 * np.sin(np.pi * t / 12)**2
+    arr_f = functools.partial(qt.poisson_random_measure, rate=rate, rate_max=4.5)
 
-        arr_f = functools.partial(qt.poisson_random_measure, rate=rate, rate_max=4.5)
+    nSamp = 15000
+    nArr = 1000
+    arrival_times = np.zeros((nSamp, nArr))
+    for k in range(nSamp):
+        t = 0
+        for j in range(nArr):
+            t = arr_f(t)
+            arrival_times[k, j] = t
+            if t > 12:
+                break
 
-        nSamp = 15000
-        nArr = 1000
-        arrival_times = np.zeros((nSamp, nArr))
-        for k in range(nSamp):
-            t = 0
-            for j in range(nArr):
-                t = arr_f(t)
-                arrival_times[k, j] = t
-                if t > 12:
-                    break
+    mu1 = 5 * np.sum(rate(np.linspace(3, 8, 200))) / 200  # or 2*(5 + (sqrt(3) + 2) * 3 / pi) + 2.5
+    mu2 = 4 * np.sum(rate(np.linspace(8, 12, 200))) / 200  # or 2*(4 - 3*sqrt(3)/pi) + 2
+    mus = [mu1, mu2]
 
-        mu1 = 5 * np.sum(rate(np.linspace(3, 8, 200))) / 200  # or 2*(5 + (sqrt(3) + 2) * 3/pi) + 2.5
-        mu2 = 4 * np.sum(rate(np.linspace(8, 12, 200))) / 200  # or 2*(4 - 3*sqrt(3)/pi) + 2
-        mus = [mu1, mu2]
+    rv1 = np.sum(np.logical_and(3 < arrival_times, arrival_times < 8), axis=1)
+    rv2 = np.sum(np.logical_and(8 < arrival_times, arrival_times < 12), axis=1)
+    rvs = [rv1, rv2]
+    df = [max(rv1) + 2, max(rv2) + 2]
 
-        rv1 = np.sum(np.logical_and(3 < arrival_times, arrival_times < 8), axis=1)
-        rv2 = np.sum(np.logical_and(8 < arrival_times, arrival_times < 12), axis=1)
-        rvs = [rv1, rv2]
-        df = [max(rv1) + 2, max(rv2) + 2]
+    Q = np.zeros((max(df), len(rvs)))
 
-        Q = np.zeros((max(df), len(rvs)))
+    for i, sample in enumerate(rvs):
+        for k in range(df[i] - 1):
+            pi_hat = nSamp * np.exp(-mus[i]) * mus[i]**k / math.factorial(k)
+            Q[k, i] = (np.sum(sample == k) - pi_hat)**2 / pi_hat
 
-        for i, sample in enumerate(rvs):
-            for k in range(df[i] - 1):
-                pi_hat = nSamp * np.exp(-mus[i]) * mus[i]**k / math.factorial(k)
-                Q[k, i] = (np.sum(sample == k) - pi_hat)**2 / pi_hat
+        ans = np.array([math.factorial(j) for j in range(k + 1)])
+        pois_cdf = np.sum(np.exp(-mus[i]) * mus[i]**np.arange(k + 1) / ans)
+        Q[k + 1, i] = nSamp * (1 - pois_cdf)
 
-            ans = np.array([math.factorial(j) for j in range(k + 1)])
-            pois_cdf = np.sum(np.exp(-mus[i]) * mus[i]**np.arange(k + 1) / ans)
-            Q[k + 1, i] = nSamp * (1 - pois_cdf)
-
-        Qs = np.sum(Q, axis=0)
-        p = np.array([1 - chi2_cdf(Qs[i], df[i] - 2) for i in range(len(rvs))])
-        self.assertTrue((p > 0.1).any())
+    Qs = np.sum(Q, axis=0)
+    p = np.array([1 - chi2_cdf(Qs[i], df[i] - 2) for i in range(len(rvs))])
+    assert (p > 0.1).any()
