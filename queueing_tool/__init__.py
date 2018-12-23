b700c2282f9af18f2c986dddd9146fecaa7b9387
Daniel <dan.jordon@gmail.com>
2018-12-22 21:39:53 -0500

    Bump the version

--- queueing_tool/__init__.py
+++ queueing_tool/__init__.py
@@ -1,20 +1,24 @@
 from __future__ import absolute_import
 
-from queueing_tool.queues import *
-import queueing_tool.queues as queues
-
-from queueing_tool.network import *
-import queueing_tool.network as network
+from queueing_tool.common import *
+import queueing_tool.common as common
 
 from queueing_tool.graph import *
 import queueing_tool.graph as graph
 
-__all__ = []
-__version__ = '1.2.2'
+from queueing_tool.network import *
+import queueing_tool.network as network
 
-__all__.extend(['__version__'])
-__all__.extend(queues.__all__)
-__all__.extend(network.__all__)
+from queueing_tool.queues import *
+import queueing_tool.queues as queues
+
+
+__all__ = []
+__version__ = '1.3.0'
+
+__all__.extend(['__version__', 'EdgeID', 'AgentID'])
 __all__.extend(graph.__all__)
+__all__.extend(network.__all__)
+__all__.extend(queues.__all__)
 
 # del queues, network, generation, graph
