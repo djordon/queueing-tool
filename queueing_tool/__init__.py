7a0fc9c4f741be4ccf6401d9e3df0e0f9d37699d
djordon <dan.jordon@gmail.com>
2022-10-02 23:07:58 -0400

    Set the version using importlib, and update the classifiers.

--- queueing_tool/__init__.py
+++ queueing_tool/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+import importlib.metadata
+
 from queueing_tool.queues import *
 import queueing_tool.queues as queues
 
@@ -9,9 +11,9 @@ import queueing_tool.network as network
 from queueing_tool.graph import *
 import queueing_tool.graph as graph
 
-__all__ = []
-__version__ = '1.2.3'
+__version__ = importlib.metadata.version(__package__ or __name__)
 
+__all__ = []
 __all__.extend(['__version__'])
 __all__.extend(queues.__all__)
 __all__.extend(network.__all__)
